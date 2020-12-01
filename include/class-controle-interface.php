a3c881ff86fb3c7c2c198c3cfcd29591f5fdbbdf
a17016@bcc.unifal-mg.edu.br <a17016@bcc.unifal-mg.edu.br>
2020-12-01 09:03:07 -0300

    colando nome usuario no inicial

--- include/class-controle-interface.php
+++ include/class-controle-interface.php
@@ -5,11 +5,12 @@ class Controle_Interface{
     const SECAO_LISTA = 'Lista';
 
     public static function exibir_cabecalho(string $titulo, string $secao){
-       $template = new raelgc\view\Template(__DIR__. '/../partial/cabecalho.html');
-       $template->TITULO = $titulo;
-       $template ->SELECIONADO_HOME = ($secao === self::SECAO_HOME) ? 'selecionado' : '';
-       $template ->SELECIONADO_LISTA = ($secao === self::SECAO_LISTA) ? 'selecionado' : '';
-       $template ->show();
+        $template = new raelgc\view\Template(__DIR__. '/../partial/cabecalho.html');
+        $template ->NOME_USUARIO_LOGADO = $_SESSION['usuario'];
+        $template->TITULO = $titulo;
+        $template ->SELECIONADO_HOME = ($secao === self::SECAO_HOME) ? 'selecionado' : '';
+        $template ->SELECIONADO_LISTA = ($secao === self::SECAO_LISTA) ? 'selecionado' : '';
+        $template ->show();
 
     }
 }
